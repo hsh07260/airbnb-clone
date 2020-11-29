a240a460af3d57ff9e82dfeddfd890a92b322321
Hahm <hsh07260@naver.com>
2020-11-29 14:14:19 +0900

    make reservation model

--- config/settings.py
+++ config/settings.py
@@ -44,6 +44,7 @@ PROJECT_APPS = [
     "users.apps.UsersConfig",
     "rooms.apps.RoomsConfig",
     "reviews.apps.ReviewsConfig",
+    "reservations.apps.ReservationsConfig",
 ]
 
 THIRTH_PARTY_APPS = ["django_countries"]
