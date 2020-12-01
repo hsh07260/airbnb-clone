37a8306d8ac2f601e84c3100a25e18092691b5da
Hahm <hsh07260@naver.com>
2020-12-01 19:48:45 +0900

    custom admin panel

--- rooms/admin.py
+++ rooms/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.html import mark_safe
 from . import models
 
 
@@ -7,7 +8,15 @@ class ItemAdmin(admin.ModelAdmin):
 
     """ Item Admin Definition """
 
-    pass
+    list_display = ("name", "used_by")
+
+    def used_by(self, obj):
+        return obj.rooms.count()
+
+
+class PhotoInline(admin.TabularInline):
+
+    model = models.Photo
 
 
 @admin.register(models.Room)
@@ -15,7 +24,67 @@ class RoomAdmin(admin.ModelAdmin):
 
     """ Room Admin Definition """
 
-    pass
+    inlines = (PhotoInline,)
+
+    fieldsets = (
+        (
+            "Basic Info",
+            {"fields": ("name", "description", "country", "address", "price")},
+        ),
+        ("Times", {"fields": ("check_in", "check_out", "instant_book")}),
+        ("More About the Space", {"fields": ("amenity", "facility", "house_rules")}),
+        ("Spaces", {"fields": ("guests", "beds", "bedrooms", "baths")}),
+        ("Last Details", {"fields": ("host",)}),
+    )
+    list_display = (
+        "name",
+        "host",
+        "country",
+        "city",
+        "price",
+        "guests",
+        "beds",
+        "bedrooms",
+        "baths",
+        "check_in",
+        "check_out",
+        "instant_book",
+        "count_amenity",
+        "count_photo",
+        "total_rating",
+    )
+
+    ordering = ("name", "price", "bedrooms")
+
+    list_filter = (
+        "instant_book",
+        "host__superhost",
+        "room_type",
+        "amenity",
+        "facility",
+        "house_rules",
+        "city",
+        "country",
+    )
+
+    raw_id_fields = ("host",)
+
+    search_fields = (
+        "city",
+        "host__username",
+    )
+
+    filter_horizontal = (
+        "amenity",
+        "facility",
+        "house_rules",
+    )
+
+    def count_amenity(self, obj):
+        return obj.amenity.count()
+
+    def count_photo(self, obj):
+        return obj.photos.count()
 
 
 @admin.register(models.Photo)
@@ -23,4 +92,12 @@ class PhotoAdmin(admin.ModelAdmin):
 
     """ Photo Admin Definition """
 
-    pass
+    list_display = (
+        "__str__",
+        "get_thumbnail",
+    )
+
+    def get_thumbnail(self, obj):
+        return mark_safe(f'<img width="50px" src="{obj.file.url}" />')
+
+    get_thumbnail.short_description = "Thumbnail"
