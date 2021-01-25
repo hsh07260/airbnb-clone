89754c3443a0565d642032c74ee2077e43b8e6c2
Hahm <hsh07260@naver.com>
2021-01-26 01:59:24 +0900

    eb setting

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = "*"
+ALLOWED_HOSTS = ["airbnb-clone.eba-jikpnpfr.ap-northeast-2.elasticbeanstalk.com"]
 
 
 # Application definition
